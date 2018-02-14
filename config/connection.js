e75a09f4d04d7a048fe2dd170af023fc48d75c72
Katy <ihatetoast@gmail.com>
2018-02-14 12:44:39 -0600

    dealt with jawsdb

--- config/connection.js
+++ config/connection.js
@@ -2,12 +2,7 @@
 // Export the connection. ... FOR THE ORM
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "ufos_db"
-});
+var connection; 
 
 connection.connect(function(err) {
   if (err) {
@@ -16,6 +11,17 @@ connection.connect(function(err) {
   }
   console.log("connected as id " + connection.threadId);
 });
+
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "ufos_db"
+  });
+}
 
 //required by ORM
 module.exports = connection;
